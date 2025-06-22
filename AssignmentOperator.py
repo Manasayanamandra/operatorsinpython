4cf5ac7fc156a1f3a503822b507836b49f18d628
Manasayanamandra <yanamandramanasa3@gmail.com>
2025-06-22 12:40:48 +0530

    Add files via upload

---
+++ AssignmentOperator.py
@@ -0,0 +1,21 @@
+'''
+USED TO ASSIGN AND UPDATE VALUES
+| Operator | Meaning             | Example                |
+| -------- | ------------------- | ---------------------- |
+| `=`      | Assign              | `x = 5`                |
+| `+=`     | Add and assign      | `x += 3` → `x = x + 3` |
+| `-=`     | Subtract and assign | `x -= 2`               |
+| `*=`     | Multiply and assign | `x *= 4`               |
+| `/=`     | Divide and assign   | `x /= 2`               |
+| `//=`    | Floor divide assign | `x //= 2`              |
+| `%=`     | Modulus and assign  | `x %= 2`               |
+| `**=`    | Power and assign    | `x **= 3`              |
+
+'''
+x = 5
+x +=3 #same as x = x+3
+print(x)
+x -=3
+print(x)
+x *=3
+print(x)
