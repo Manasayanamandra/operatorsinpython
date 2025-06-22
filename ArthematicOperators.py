c2e0def6e1b0aa18496ddd8469a34b5c32addce8
Manasayanamandra <yanamandramanasa3@gmail.com>
2025-06-22 12:40:11 +0530

    Add files via upload

---
+++ ArthematicOperators.py
@@ -0,0 +1,34 @@
+'''
+USED FOR BASIC MATHS:
+
+| Operator | Meaning        | Example       |
+| -------- | -------------- | ------------- |
+| `+`      | Addition       | `2 + 3 = 5`   |
+| `-`      | Subtraction    | `5 - 2 = 3`   |
+| `*`      | Multiplication | `2 * 3 = 6`   |
+| `/`      | Division       | `6 / 2 = 3.0` |
+| `//`     | Floor Division | `7 // 2 = 3`  |
+| `%`      | Modulus        | `7 % 2 = 1`   |
+| `**`     | Exponentiation | `2 ** 3 = 8`  |
+
+'''
+a = 10
+b = 3
+print("Addition", a+b)
+print("Subtraction", a-b)
+print("Multiplication", a*b)
+print("Division", a/b)
+print("Floor Division", a//b)
+print("Modulus", a%b)
+print("Exponentiation", a**b)
+
+'''
+OUTPUT:
+Addition 13
+Subtraction 7
+Multiplication 30
+Division 3.3333333333333335
+Floor Division 3
+Modulus 1
+Exponentiation 1000
+'''
