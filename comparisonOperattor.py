55f5f40ef459c854356de317b35048f3f8fe4979
Manasayanamandra <yanamandramanasa3@gmail.com>
2025-06-22 12:44:53 +0530

    Add files via upload

---
+++ comparisonOperattor.py
@@ -0,0 +1,20 @@
+'''
+| Operator | Meaning          | Example         |
+| -------- | ---------------- | --------------- |
+| `==`     | Equal to         | `5 == 5 → True` |
+| `!=`     | Not equal to     | `5 != 3 → True` |
+| `>`      | Greater than     | `4 > 2 → True`  |
+| `<`      | Less than        | `3 < 5 → True`  |
+| `>=`     | Greater or equal | `5 >= 5 → True` |
+| `<=`     | Less or equal    | `3 <= 5 → True` |
+
+
+ '''
+a = 10
+b = 20
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
